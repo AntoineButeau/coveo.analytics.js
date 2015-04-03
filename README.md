1d16b0e61882901a562a5c2f0ec8b4bd8b8f3391
Pierre-Alexandre St-Jean <pa@stjean.me>
2015-04-03 16:50:38 -0400

    refactoring, adding .editorconfig (because 4spaces ), readme is gonna change, some changes in eslint )

--- README.md
+++ README.md
@@ -4,69 +4,18 @@
 
 Send events to the Coveo Analytics service. No external dependencies, pure JavaScript solution.
 
-# TL;DR
-To send analytics events when your page is loaded, paste the following snippet into your html page so that it appears before the closing `</head>` tag.
-```html
-<script src="https://static.cloud.coveo.com/ua/coveo.analytics.min.js"></script>
-<script>
-Coveo.UA.token = 'your-token-here';
-Coveo.UA.sendCustomEvent({
-    eventType: 'EventType',
-    eventValue: 'EventValue'
-});
-</script>
-```
-Replace `your-token-here` with your API Key. Change `EventType` and `EventValue` to any custom event you want to send.
-
 # Usage
-## Include in your html files.
 
-Using our CloudFront CDN:
-```html
-<script src="https://static.cloud.coveo.com/ua/coveo.analytics.min.js"></script>
-```
-
-Using locally:
-```html
-<script src="path/to/coveo.analytics.min.js"></script>
-```
-
-## Set your access token
-Ask your administrator for an API key
+in Node:
 
 ```js
-Coveo.UA.token = 'aaaaaaaa-bbbbbbbb-cccccccc-ddddddddd';
-```
 
-## Send events
-```js
-Coveo.UA.sendCustomEvent({
-    eventType: 'Your event type here',
-    eventValue: 'Event Value'
+var CoveoUA = require('coveo-usageanalytics');
+
+var ua = CoveoUA({token: 'your-token-here'});
+
+ua.sendCustomEvent({
+  eventType: 'Your Custom Event Type',
+  eventValue: 'Your Custom Event Value'
 });
 ```
-
-## Optional parameters
-```js
-{
-    eventType: string,
-    eventValue: string,
-    lastSearchQueryUid: string,
-    anonymous: boolean,
-    userGroups: array[string],
-    userDisplayName: string,
-    customData: {
-        my_data_x: 'custom dimension value 1',
-        some_other_data_y: 'custom dimension value 2'
-    },
-    device: string,
-    mobile: boolean,
-    splitTestRunName: string,
-    splitTestRunVersion: string,
-    userAgent: string,
-    username: string,
-    language: string
-}
-```
-
-For more info, see https://usageanalytics.coveo.com/docs/#!/v13_analytics/addCustomEventViaPost
--- coveo.analytics.js
+++ coveo.analytics.js
@@ -4,43 +4,47 @@ var UA = {};
 
 // Let's be able to use it in the browser
 if (typeof module !== 'undefined' && module.exports) {
-    module.exports=Coveo.UA;
+    module.exports = Coveo.UA;
 } else {
-    var Coveo = Coveo || {};
+    var Coveo = Coveo || { };
     Coveo.UA = UA;
 }
 
 
 var _defaults = {
-  version:13,
-  endpoint:'https://usageanalytics.coveo.com'
+    version:  13,
+    endpoint: 'https://usageanalytics.coveo.com'
 };
 
 
 function CoveoUsageAnalytics(options){
-  // Do not require new when calling
-  if(!(this instanceof CoveoUsageAnalytics)){
-    return new CoveoUsageAnalytics(options);
-  }
+    // Do not require new when calling
+    if(!(this instanceof CoveoUsageAnalytics)){
+        return new CoveoUsageAnalytics(options);
+    }
 
-  this.version  = options.version || _defaults.version;
-  this.endpoint = options.endpoint || _defaults.endpoint;
-  this.token    = options.token;
+
+    this.version = options.version || _defaults.version;
+    this.endpoint = options.endpoint || _defaults.endpoint;
+    this.token = options.token;
+
+    // TODO: GENERATE A SESSION ID HERE?
+    // or we save the first one the analytics service returns us
 }
 
 // Queries
 // get status : gets the status of the analytics service
-CoveoUsageAnalytics.prototype.status = function(){
-  // TODO: do the query
-  return false;
-}
+CoveoUsageAnalytics.prototype.getStatus = function(){
+    // TODO: do the query
+    return { status: false };
+};
 // post search : add a search event
-CoveoUsageAnalytics.prototype.search = function(options){ return false }
+CoveoUsageAnalytics.prototype.sendSearchEvent = function(){ return false; };
 // post searches : add multiple seaches
-CoveoUsageAnalytics.prototype.searches = function(options){ return false }
+CoveoUsageAnalytics.prototype.sendSearchEvents = function(){ return false; };
 // post click : add click event
-CoveoUsageAnalytics.prototype.click = function(options){ return false }
+CoveoUsageAnalytics.prototype.sendClickEvent = function(){ return false; };
 // post custom : add custom events
-CoveoUsageAnalytics.prototype.custom = function(options){ return false }
+CoveoUsageAnalytics.prototype.sendCustomEvent = function(){ return false; };
 // delete session : clears cookies
-CoveoUsageAnalytics.prototype.deleteSession = function(options){ return false }
+CoveoUsageAnalytics.prototype.deleteSession = function(){ return false; };
